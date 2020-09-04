11e810cd3d4cddeda7a556260342c8a0c9a2ce14
evanRubinsteinIT <70485623+evanRubinsteinIT@users.noreply.github.com>
2020-09-03 21:48:44 -0700

    Delete Main.py

--- Main.py
+++
@@ -1,7 +0,0 @@
-from PDF_Text_Extractor import extractEmail
-from EmailSender import sendEmail
-#Ask user for what PDF they want to use
-pdfUserChoice = input("Please input the file you would like to use: ")
-#Call functions from other files and use input from above question
-extractEmail(pdfUserChoice)
-sendEmail(pdfUserChoice)
